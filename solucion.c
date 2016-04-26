c6156dac0a78cb821a20c61532067c5c70a3a688
Sandra Jimena Gonzalez Lozano <sj.gonzalez10@uniandes.edu.co>
2016-04-26 11:20:23 -0500

    agregue funcion que escribe posiciones y otra que evoluciona el sistema

--- solucion.c
+++ solucion.c
@@ -43,7 +43,6 @@ double leap_frog(double delta_t, double t, double *x, double *y, double *z, doub
   /* for sobre cada particula */
   int i;
   for(i=0; i < n_bodies; i++){
-    /*magnitud radio*/
     /*kick*/
     deriv_vx = deriv_v(t, x[i], vx[i], x[i], y[i], z[i], x, y, z, n_bodies);
     deriv_vy = deriv_v(t, y[i], vy[i], x[i], y[i], z[i], x, y, z, n_bodies);
@@ -62,16 +61,42 @@ double leap_frog(double delta_t, double t, double *x, double *y, double *z, doub
       vx_in += 0.5 * deriv_vx * delta_t;
       vy_in += 0.5 * deriv_vy * delta_t;
       vz_in += 0.5 * deriv_vz * delta_t;
-  
-      /*actualiza*/
+     /*actualiza*/
       x[i] = x_in;
       y[i] = y_in;
       z[i] = z_in;
       vx[i] = vx_in;
       vy[i] = vy_in;
       vz[i] = vz_in;
-
-  }
+}
   return 0;
+}
 
+/* escribe posiciones particulas*/
+void escribe_estado(double *x, double *y, double *z, int n_bodies, int id){
+  FILE *out;
+  char filename[512];
+  int i; 
+  sprintf(filename, "output_%d.dat", id);
+  if(!(out=fopen(filename, "w"))){
+    fprintf(stderr, "Problem opening file %s\n", filename);
+    exit(0);
+  }
+  for(i=0; i < n_bodies; i++){
+    fprintf(out, "%f %f %f \n", x[i], y[i], x[i]);
+  }
+  fclose(out);
+}
+
+void evoluciona_sistema(double t_total, double delta_t, double *x, double *y, double *z, double *vx, double *vy, double *vz, double n_bodies){
+  double t;
+  int n_step = (int)(t_total/delta_t);
+  int i;
+  t = 0.0;
+
+  for(i=0; i < n_bodies; i++){
+    escribe_estado(x, y, z, n_bodies, i);
+    leap_frog(delta_t, t, x, y, z, vx, vy, vz, n_bodies);
+    t += delta_t;
+  }
 }
--- solucion.h
+++ solucion.h
@@ -2,3 +2,5 @@ double calcular_masa(double rx, double ry, double rz, double *x, double *y, doub
 double deriv_r(double t, double r, double v);
 double deriv_v(double t, double r, double v, double rx, double ry, double rz, double *x, double *y, double *z, double n_bodies);
 double leap_frog(double delta_t, double t, double *x, double *y, double *z, double *vx, double *vy, double *vz, double n_bodies);
+void escribe_estado(double *x, double *y, double *z, int n_bodies, int id);
+void evoluciona_sistema(double t_total, double delta_t, double *x, double *y, double *z, double *vx, double *vy, double *vz, double n_bodies);
