1f080376e08a04d12ff23796121b2120151c584e
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2016-04-25 19:22:22 -0500

    funciones inicializacion

---
+++ iniciar.h
@@ -0,0 +1,4 @@
+void init_positions(double *x, double *y, double *z, double n_bodies);
+void init_vectors(double *x, double *y, double *z, double n_bodies);
+void recibe_input(int argc, char **argv, int *n_bodies, double *epsilon);
+double *crea_vector(int n_bodies);
---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "iniciar.h"
+
+int main(int argc, char **argv){
+
+  int i = 0;
+  int n_bodies = 0;
+  double epsilon = 0;
+  double *x = NULL, *y = NULL, *z = NULL; //Vectores posicion
+  double *v_x = NULL, *v_y = NULL, *v_z = NULL;  //Vectores velocidad
+  double *a_x = NULL, *a_y = NULL, *a_z = NULL;  //Vectores aceleracion
+
+  //Inicializacion de vectores
+  recibe_input(argc, argv, &n_bodies, &epsilon);
+  x = crea_vector(n_bodies);
+  y = crea_vector(n_bodies);
+  z = crea_vector(n_bodies);
+  v_x = crea_vector(n_bodies);
+  v_y = crea_vector(n_bodies);
+  v_z = crea_vector(n_bodies);
+  a_x = crea_vector(n_bodies);
+  a_y = crea_vector(n_bodies);
+  a_z = crea_vector(n_bodies);  
+
+  init_positions(x, y, z, n_bodies); 
+ 
+  for(i=0 ; i<n_bodies ; i++){
+
+    printf("%f \t %f \t %f \n",  x[i],  y[i],  z[i]);
+
+  }
+  
+  return 0;
+}
