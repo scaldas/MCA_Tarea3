e3fc6c2bb6b5406182895a58607def360cd6a7fb
Sandra Jimena Gonzalez Lozano <sj.gonzalez10@uniandes.edu.co>
2016-04-26 10:14:26 -0500

    casi completo leap_frog

--- solucion.c
+++ solucion.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#define G_GRAV 4.49E-3
 
 /*
 calcula masa que esta entre el centro y el punto
@@ -25,9 +26,10 @@ double deriv_r(double t, double r, double v){
   return v;
 }
 
-double deriv_v(double t, double r, double v){
-  double c = -1;
-  return c*r;
+double deriv_v(double t, double r, double v, double rx, double ry, double rz, double *x, double *y, double *z, double n_bodies){
+  double m = calcular_masa(rx, ry, rz, x, y, z, n_bodies);
+  double r_mag = sqrt( pow(rx, 2) + pow(ry, 2) + pow(rz, 2));
+  return (-G*m*r)/ pow(r_mag, 3);
 }
 
 /*
@@ -37,19 +39,27 @@ evoluciona todas las particulas en cada componente
 double leap_frog(double delta_t, double t, double *x, double *y, double *z, double *vx, double *vy, double *vz, double n_bodies){
   double x_in, y_in, z_in;
   double vx_in, vy_in, vz_in;
+  double m_temp;
   /* for sobre cada particula */
   int i;
   for(i=0; i < n_bodies; i++){
     /*magnitud radio*/
-    double r = sqrt(pow(x[i],2) + pow(y[i],2) + pow(z[i],2));
     /*kick*/
-      vx_in += (0.5 * x[i])/pow(r, 3) * delta_t;
-      vy_in += (0.5 * y[i])/pow(r, 3) * delta_t;
-      vz_in += (0.5 * z[i])/pow(r, 3) * delta_t;
+    double deriv_vx = deriv_v(t, x[i], vx[i], x[i], y[i], z[i], x, y, z);
+    double deriv_vy = deriv_v(t, y[i], vy[i], x[i], y[i], z[i], x, y, z);
+    double deriv_vz = deriv_v(t, z[i], vz[i], x[i], y[i], z[i], x, y, z);
+      vx_in += 0.5 * deriv_vx * delta_t;
+      vy_in += 0.5 * deriv_vy * delta_t;
+      vz_in += 0.5 * deriv_vz * delta_t;
       /*drift*/
       x_in += 1.0 * vx_in * delta_t;
       y_in += 1.0 * vy_in * delta_t;
       z_in += 1.0 * vz_in * delta_t;
+      /*kick*/
+      deriv_vx = deriv_v(t, x_in, vx_in, x[i], y[i], z[i], x, y, z);
+      deriv_vy = deriv_v(t, y_in, vy_in, x[i], y[i], z[i], x, y, z);
+      deriv_vz = deriv_v(t, z_in, vz_in, x[i], y[i], z[i], x, y, z);
+
   }
   return 0;
 
