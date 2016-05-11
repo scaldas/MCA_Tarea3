eca0c0f80b5ad67529a4555a4e1b22d698f32f71
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2016-05-10 21:40:33 -0500

    numero de threads como parametro

--- src/inicial.h
+++ src/inicial.h
@@ -2,4 +2,4 @@ void escribe_estado(double *x, double *y, double *U, double *K, int n, int id);
 void posiciones_iniciales(double *p, int n);
 double *crea_vector(int n);
 int *crea_vector_int(int n);
-void recibe_input(int argc, char **argv, int *n, double *e, int *num_buckets);
+void recibe_input(int argc, char **argv, int *n, double *e, int *num_buckets, int *num_threads);
